85160145bc2c72ae5e8a0a1bbb681c8ac5fcca18
Denovo <vo0v0000@naver.com>
2023-10-10 23:52:37 +0900

    chore: test 폴더 이동

---
+++ src/test/unit/app.controller.spec.ts
@@ -0,0 +1,12 @@
+describe('AppController', () => {
+  describe('init', () => {
+    it('should return true', () => {
+      expect(true).toBe(true);
+    });
+  });
+  describe('health', () => {
+    it('should return OK', () => {
+      expect('OK').toBe('OK');
+    });
+  });
+});
